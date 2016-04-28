3c92346a1a1d4650c1dd6b8b8818883efc9b146b
skarudvy <skarudvy0202@gmail.com>
2016-04-29 01:35:45 +0900

    algospot cs version

---
+++ 03.BRACKETS2/BRACKETS2/Brackets.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BRACKETS2
+{
+    public class Brackets
+    {
+        private string bracket_list = "";
+        Stack<char> st = new Stack<char>();
+
+        public Brackets(string bracket_list)
+        {
+            this.bracket_list = bracket_list;
+        }
+
+        private bool isStartBracket(char bracket)
+        {
+            if (bracket == '(' || bracket == '{' || bracket == '[')
+                return true;
+            else
+                return false;
+        }
+
+        private bool isMatchBracket(char start, char end)
+        {
+            if (start == '(' && end == ')')
+                return true;
+            else if (start == '{' && end == '}')
+                return true;
+            else if (start == '[' && end == ']')
+                return true;
+            else
+                return false;
+        }
+
+        public bool ValidationBracket()
+        {
+            int length = bracket_list.Length;
+            if (length % 2 != 0)
+                return false;
+
+            for (int bracket_index = 0; bracket_index < length; bracket_index++)
+            {
+                if (isStartBracket(bracket_list[bracket_index]))
+                {
+                    st.Push(bracket_list[bracket_index]);
+                }
+                else
+                {
+                    if (st.Count != 0 && isMatchBracket(st.Peek(), bracket_list[bracket_index]))
+                    {
+                        st.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 03.BRACKETS2/BRACKETS2_TEST2/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BRACKETS2;
+using NUnit.Framework;
+
+namespace BRACKETS2_TEST2
+{
+    public class UnitTest1
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddingAccountToAccountsAddsAccountToList()
+        {
+            Brackets brackets = new Brackets("()()");
+            Assert.AreEqual(brackets.ValidationBracket(), true);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
